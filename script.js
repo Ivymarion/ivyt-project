0e603dd45c7453a5dd1e0ea1cfd16af72f8598e5
Ivymarion <ivymarion2013@gmail.com>
2022-10-15 16:11:24 +0300

    json

---
+++ script.js
@@ -0,0 +1,31 @@
+let menu =document.querySelector("#loginbox");
+let navbar = document.querySelector('avatar');
+menu.onclick = () =>{
+    menu.classList.toggle("avatar")
+}
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.URL);
+
+console.log(document.images);
+console.log(document.doctype);
+console.log(document.all);
+console.log(document.all[9].textContent='Please text');
+
+getElementById//
+console.log(document.getElementById('header title'));
+console.log(headerTitle);
+headerTitle.textContent='ivy t borana';
+headerTitle.innerText='breed selling';
+console.log(headerTitle.innerText);
+
+var my_goal= document.getElementsByClassName('it');
+for (var r = 0; r < FileList.length; r++){
+    div[r].style.backgroundColor='#eee';
+}
+var a=document.getElementsByTagName('a');
+console.log(a);
+console.log(a[1]);
+a[1].textContent='your price';
+a[1].style.fontFamily='Times New Roman';
+a[1].style.alignContent='centre';
